1854882b8587dfa75ddb0359de9092759dbbb566
shreyaskn72 <shreyaskn72@gmail.com>
2024-04-16 23:56:00 +0530

    list meeting types

--- oauth_calendly/0.3/app5.py
+++ oauth_calendly/0.3/app5.py
@@ -4,14 +4,79 @@ import requests
 app = Flask(__name__)
 
 # Your Calendly OAuth 2.0 credentials
-CLIENT_ID = 'client id'
-CLIENT_SECRET = 'client secret'
+CLIENT_ID = 'client_id'
+CLIENT_SECRET = 'client_secret'
 
 # Set the redirect URL for your application
 REDIRECT_URI = 'http://localhost:8080'  # Change this to your desired redirect URL
 
 AUTHORIZE_URL = 'https://calendly.com/oauth/authorize'
 TOKEN_URL = 'https://calendly.com/oauth/token'
+
+
+def get_access_token(refresh_token):
+    payload = {
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+    }
+    response = requests.post(TOKEN_URL, data=payload)
+    if response.status_code == 200:
+        output = response.json()
+        print(output)
+        return output.get('access_token')
+    else:
+        print("Error occurred while getting access token:")
+        print("Status code:", response.status_code)
+        print("Response:", response.json())
+        return None
+
+def list_meeting_types(access_token, organization):
+    url = 'https://api.calendly.com/event_types'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+    params = {
+        'organization': organization
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        for event_type in data['collection']:
+            print(event_type)
+            print(event_type['name'])
+    else:
+        print("Error occurred while fetching meeting types:")
+        print("Status code:", response.status_code)
+        print("Response:", response.json())
+
+    output_event_types = response.json()
+
+    return output_event_types
+
+
+def user_me(access_token):
+    # Use the access token to make requests to the Calendly API
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    # Fetch user details
+    user_response = requests.get('https://api.calendly.com/users/me', headers=headers)
+
+    user_data = user_response.json()
+
+    print("user_data")
+
+    print(user_data)
+
+    return user_data
+
+
 
 @app.route('/')
 def home():
@@ -34,29 +99,39 @@ def callback():
         refresh_token = data['refresh_token']
         access_token = get_access_token(refresh_token)
         if access_token:
-            return f"Access Token: {access_token}"
+            return data
         else:
             return "Error occurred while getting access token."
     else:
         return "Error occurred while exchanging authorization code for tokens."
 
-def get_access_token(refresh_token):
-    payload = {
-        'grant_type': 'refresh_token',
-        'refresh_token': refresh_token,
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET
-    }
-    response = requests.post(TOKEN_URL, data=payload)
-    if response.status_code == 200:
-        output = response.json()
-        print(output)
-        return output.get('access_token')
-    else:
-        print("Error occurred while getting access token:")
-        print("Status code:", response.status_code)
-        print("Response:", response.json())
-        return None
+@app.route('/get_users_data', methods=["POST"])
+def get_users_data():
+    access_token = request.json['access_token']
+    user_data = user_me(access_token)
+    return user_data
+
+@app.route("/get_event_types", methods=["POST"])
+def get_event_types():
+    access_token = request.json['access_token']
+    user_data = user_me(access_token)
+    print("type of user data is")
+    print(type(user_data))
+    print(user_data)
+    current_organization = user_data["resource"]["current_organization"]
+
+    output_event_types = list_meeting_types(access_token, current_organization)
+
+    event_type_list = []
+
+    for event_type in output_event_types['collection']:
+        event_type_list.append(event_type['name'])
+
+    return event_type_list
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
